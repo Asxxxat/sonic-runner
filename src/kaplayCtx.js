e2ba6d449facf653ad4e763238fd9692481fd1c5
Askhat Zhankulov <askhat1008@gmail.com>
2025-07-08 18:27:06 +0500

    first commit

---
+++ src/kaplayCtx.js
@@ -0,0 +1,22 @@
+import kaplay from "kaplay";
+
+const k = kaplay({
+    width: 1920,
+    height: 1080,
+    stretch: true,
+    letterbox: false,
+    background: [0, 0, 0],
+    global: false,
+    buttons: {
+        jump: {
+            keyboard: ["space"],
+            mouse: "left",
+        },
+    },
+    touchToMouse: true,
+    debug: false,
+});
+
+document.getElementById("game-wrapper").appendChild(k.canvas);
+
+export default k;
